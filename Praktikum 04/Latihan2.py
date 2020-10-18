4a432d396caaf7ed4c8d3bc268352ffc9dc49275
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-10-18 20:54:26 +0700

    Create Latihan2.py

---
+++ Praktikum 04/Latihan2.py
@@ -0,0 +1,5 @@
+print(" Bensin Yang Diperlukan Pak Budi Untuk Perjalanan Dari Kota A ke Kota B ")
+jarak = int(input("Jarak : "))
+jarakperbensin = 12
+bensin = jarak/jarakperbensin
+print(" Bensin Yang Dibutuhkan : ",bensin , " Liter")
