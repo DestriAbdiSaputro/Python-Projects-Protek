eb7b1cdfa442549394df71701f1c818f7497f805
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-10-18 20:54:40 +0700

    Create Latihan5.py

---
+++ Praktikum 04/Latihan5.py
@@ -0,0 +1,5 @@
+print(" Data Jumlah Anak Laki-Laki dan Perempuan dari Mahasiswa PTIK UNS ")
+laki = '+'*45
+print(" Laki-Laki : ", laki , ("(45)"))
+perempuan = '+'*35
+print(" Perempuan : ", perempuan , ("(35)"))
