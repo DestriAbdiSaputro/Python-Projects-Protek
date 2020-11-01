ce246206d07b425d9d6607fb0c6231bbbbabd3bb
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-01 13:56:00 +0700

    Update prak4.py

--- Praktikum 05/Praktikum2/prak4.py
+++ Praktikum 05/Praktikum2/prak4.py
@@ -3,10 +3,9 @@ kolom = 5
 baris = 5
 i = 0
 while (i < baris):
-    i += 1
-    j = 5
+    j = 0
     while (j < kolom):
         print('*', end='')
         j += 1
     print('')
-    
+    i += 1
