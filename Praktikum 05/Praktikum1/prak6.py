3899b18d8750eb3855a1e5aaa219e8850cce1418
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-01 13:27:33 +0700

    Create prak6.py

---
+++ Praktikum 05/Praktikum1/prak6.py
@@ -0,0 +1,11 @@
+a = -2
+b =-7
+if (a > 0) and (b > 0):
+    print("Keduanya positif")
+if (a > 0) or (b > 0):
+    print("Salah satunya positif")
+if (a < 0) or (b < 0):
+    print("Salah satunya negatif")
+if (a < 0) and (b < 0):
+    print("Keduanya negatif")
+
