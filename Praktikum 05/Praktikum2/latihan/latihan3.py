3f8a05bdca02906731f0876aad7efe5d6abda647
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-01 13:28:03 +0700

    Create latihan3.py

---
+++ Praktikum 05/Praktikum2/latihan/latihan3.py
@@ -0,0 +1,9 @@
+banyak = 0
+jumlah = 0
+for i in range(1, 101, 2):
+    banyak += 1
+    jumlah += i
+    print(i)
+
+print("Banyaknya bilangan ganjil : ", banyak)
+print("Jumlah bilangan ganjil : ", jumlah)
