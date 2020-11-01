9a38a380efa02219451be725c26d2aceddb54b80
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-01 14:15:44 +0700

    Create operasi.py

---
+++ Praktikum 06/Praktikum 3/operasi.py
@@ -0,0 +1,8 @@
+from operation import*
+
+a = 10
+b = 7
+print(a, "+", b, "=", jumlah(a, b))
+print(a, "-", b, "=", kurang(a, b))
+print(a, "x", b, "=", kali(a, b))
+print(a, "/", b, "=", bagi(a, b))
