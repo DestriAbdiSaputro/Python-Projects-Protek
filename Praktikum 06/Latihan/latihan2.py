d48146d35cb4b93b49a620754cdeed602ce98a71
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-01 17:53:16 +0700

    Create latihan2.py

---
+++ Praktikum 06/Latihan/latihan2.py
@@ -0,0 +1,17 @@
+def starFormation1(n):
+    for n in range (n):
+        for j in range (n+1):
+            print('*',end='')
+        print()
+def starFormation2(n):
+    for n in reversed(range (n)):
+        for j in range (n+1):
+            print('*',end='')
+        print()
+def starFormation3(n):
+    if n%2==0:
+        starFormation1(n//2)
+    else:
+        starFormation1(n//2+1)
+    starFormation2(n//2)
+starFormation3(3)
