0c409ec9c779ea4902a7a70a560fa0bba222ae38
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-01 13:28:51 +0700

    Create prak5.py

---
+++ Praktikum 05/Praktikum2/prak5.py
@@ -0,0 +1,14 @@
+#kotak bintang ajaib
+kolom = 5
+baris = 6
+
+i = 1
+while (i < baris):
+    j = 0
+    while (j in range (i)):
+        print('*', end=' ')
+        j += 1
+    print('')
+    i += 1
+
+    
