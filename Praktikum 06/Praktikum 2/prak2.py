69f58944378dfedcc37b6c0e29fa1193d0573a95
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-01 14:15:30 +0700

    Create prak2.py

---
+++ Praktikum 06/Praktikum 2/prak2.py
@@ -0,0 +1,9 @@
+def luasSegitiga2(alas, tinggi):
+    luas = alas * tinggi / 2 
+    (print("Luas segitiga dg alas ", alas,
+          " dan tinggi ", tinggi,
+          " adalah ", luas))
+
+alas = 10
+tinggi = 20
+luasSegitiga2(alas, tinggi)
