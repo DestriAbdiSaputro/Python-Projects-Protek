4ba1b11a08ebd80ed0cbdfaddadad6f1ac9367bf
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-01 14:30:43 +0700

    Create latihan1.py

---
+++ Praktikum 06/Latihan/latihan1.py
@@ -0,0 +1,25 @@
+def isPythagoras(a,b,c):
+    hasil = a**2+b**2==c**2
+    foo = True
+    bar = False
+    print(no,'. a=',a,',b=',b,',c=',c,'  ->',hasil)
+no = 1
+a = 3
+b = 4
+c = 5
+isPythagoras(a,b,c)
+no = 2
+a = 5
+b = 9
+c = 12
+isPythagoras(a,b,c)
+no = 3
+a = 8
+b = 6
+c = 10
+isPythagoras(a,b,c)
+no = 4
+a = 7
+b = 8
+c = 11
+isPythagoras(a,b,c)
