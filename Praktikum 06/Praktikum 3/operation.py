00c0526b68a1e1b8843976053e5f6550c15da837
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-01 14:15:52 +0700

    Create operation.py

---
+++ Praktikum 06/Praktikum 3/operation.py
@@ -0,0 +1,15 @@
+def jumlah(a, b):
+    hasil = a + b
+    return hasil
+
+def kali(a, b):
+    hasil = a * b
+    return hasil
+
+def kurang(a, b):
+    hasil = a - b
+    return hasil
+
+def bagi(a, b):
+    hasil = a / b
+    return hasil
