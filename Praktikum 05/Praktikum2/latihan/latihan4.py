7875f01b77b611b0919442f87cfc34e923c6566c
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-01 13:28:09 +0700

    Create latihan4.py

---
+++ Praktikum 05/Praktikum2/latihan/latihan4.py
@@ -0,0 +1,16 @@
+#kotak bintang ajaib
+kolom = 5
+baris = 6
+
+i = 5
+while (i < baris):
+    j = 0
+    while j in (range (i)):
+        print('*', end=' ')
+        j += 1
+    print('')
+    i -= 1
+    if i==0:
+        break
+
+    
