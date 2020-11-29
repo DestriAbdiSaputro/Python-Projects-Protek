41ea2dd493035c626a9bcfd55b87552bf57d7e15
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-29 23:37:21 +0700

    Create 13.py

---
+++ Praktikum 08/python project/13.py
@@ -0,0 +1,9 @@
+print('----------------------------------')
+print('            Toko Buah             ')
+print('----------------------------------')
+nilaiMhs = [{'nim' : 'A01', 'nama' : 'Amir', 'mid' : 50, 'uas' : 80},
+            {'nim' : 'A02', 'nama' : 'Budi', 'mid' : 40, 'uas' : 90},       
+            {'nim' : 'A03', 'nama' : 'Cici', 'mid' : 50, 'uas' : 50}, 
+            {'nim' : 'A04', 'nama' : 'Dedi', 'mid' : 20, 'uas' : 30},
+            {'nim' : 'A05', 'nama' : 'Fifi', 'mid' : 70, 'uas' : 40}]
+print(nilaiMhs.get('mid'))
