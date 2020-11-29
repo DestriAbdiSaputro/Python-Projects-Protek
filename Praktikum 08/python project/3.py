c239db45033f4e658adc34a844606fa047b08a9b
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-29 23:37:36 +0700

    Create 3.py

---
+++ Praktikum 08/python project/3.py
@@ -0,0 +1,12 @@
+print('----------------------------------')
+print('         Nama Mahasiswa            ')
+print('----------------------------------')
+data = []
+n = int(input('Jumlah Mahasiswa : '))
+while True:
+    nama = str(input('Nama Mahasiswa : '))
+    data = data +[nama]
+    data = data + len[nama]
+    data.sort()
+    print(data)
+    
