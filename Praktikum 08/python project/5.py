18fc7407b09606655b533d4d7da1a3842be87e90
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-11-29 23:37:44 +0700

    Create 5.py

---
+++ Praktikum 08/python project/5.py
@@ -0,0 +1,12 @@
+print('----------------------------------')
+print('             Kuadrat             ')
+print('----------------------------------')
+data = [1,2,3]
+sum = 0
+def kuadrat():
+    data = [1,2,3]
+    for i in range (data):
+        sum = data[i]**2
+        print(data)
+        
+kuadrat()
