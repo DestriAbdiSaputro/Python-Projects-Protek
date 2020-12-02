fa272636ba5f5824bef0e372568d07b42bab4da4
Destri Abdi Saputro <71741928+DestriAbdiSaputro@users.noreply.github.com>
2020-12-02 19:57:43 +0700

    Update langkahkerja2.py

--- Praktikum 08/langkahkerja2.py
+++ Praktikum 08/langkahkerja2.py
@@ -1,3 +1,9 @@
 myString = 'python adalah bahasa pemrograman yang menyenangkan'
 karakterhuruf = set(myString)
 print(karakterhuruf)
+print('='*90)
+myString = 'python adalah bahasa pemrograman yang menyenangkan'
+karakterhuruf = set(myString)
+list = list(karakterhuruf)
+list.sort()
+print(list)
