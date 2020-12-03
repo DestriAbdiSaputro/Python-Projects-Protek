9c4b51c01314b02975dc33327c9e554a1c4a8bda
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-12-03 14:41:19 +0700

    Create langkahkerja2.py

---
+++ Praktikum 08/langkahkerja2.py
@@ -0,0 +1,11 @@
+print(' '*30,'Langkah Kerja 9 s/d 11')
+print('='*90)
+myString = 'python adalah bahasa pemrograman yang menyenangkan'
+karakterhuruf = set(myString)
+print(karakterhuruf)
+print('='*90)
+myString = 'python adalah bahasa pemrograman yang menyenangkan'
+karakterhuruf = set(myString)
+list = list(karakterhuruf)
+list.sort()
+print(list)
