a6bf2a95eb9a1bb45a2b7c923d4fa685f29dfb96
Destri Abdi Saputro <destriabdi20@gmail.com>
2020-12-03 14:41:38 +0700

    Create 5.py

---
+++ Praktikum 08/python project/5.py
@@ -0,0 +1,12 @@
+print('----------------------------------')
+print('             Kuadrat             ')
+print('----------------------------------')
+def kuadrat(bil):
+    hasil = []
+    for i in bil:
+        hasil.append(i**2)
+    print(hasil)
+    
+bil =[2, 4, 5, 6]
+print('bil =',bil)
+kuadrat(bil)
