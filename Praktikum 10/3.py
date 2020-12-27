e9d7f06f59025a523bc58a06312ab3225e84686c
Destri Abdi Saputro <71741928+DestriAbdiSaputro@users.noreply.github.com>
2020-12-27 18:13:26 +0700

    Update 3.py

--- Praktikum 10/3.py
+++ Praktikum 10/3.py
@@ -3,6 +3,7 @@ data  = file.readlines()
 hasil = []
 for i in data:
     pisah = i.split('|')
+    pisah[2] = pisah[2].rstrip('\n')
     dict  = {'nim' : pisah[0], 'nama' : pisah[1], 'alamat' : pisah[2]}
     hasil += [dict]
 print('dataMhs = ',hasil)
